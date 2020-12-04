0bd4b619ffe465181275af1c7144653b4ba4496f
Angelika <angelika.stryzhych@billwerk.com>
2020-12-04 20:50:32 +0300

    Add keyword specific files

--- NLIIS_Autoreferer/Services/KeywordReferer.cs
+++ NLIIS_Autoreferer/Services/KeywordReferer.cs
@@ -1,9 +1,23 @@
-﻿namespace NLIIS_Autoreferer.Services
+﻿using OpenTextSummarizer;
+
+namespace NLIIS_Autoreferer.Services
 {
     public class KeywordReferer : IReferer
     {
         public string GenerateRefer(string text)
         {
+            SummarizerArguments sumargs = new SummarizerArguments
+            {
+                DictionaryLanguage = "en",
+                DisplayLines = sentCount,
+                DisplayPercent = 0,
+                InputFile = "",
+                InputString = OriginalTextBox.Text // here your text
+            };
+            SummarizedDocument doc = Summarizer.Summarize(sumargs);
+            string summary = string.Join("\r\n\r\n", doc.Sentences.ToArray());
+// do some stuff with summary. It is your result.
+            
             var words = DocumentService.GetTerms(text);
             return null;
         }
---
+++ NLIIS_Autoreferer/Services/KeywordSpecific/SummarizedDocument.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace NLIIS_Autoreferer.Services.KeywordSpecific
+{
+    public class SummarizedDocument
+    {
+        public List<string> Concepts { get; set; }
+        public List<string> Sentences { get; set; }
+
+        internal SummarizedDocument()
+        {
+            Sentences = new List<string>();
+            Concepts = new List<string>();
+        }
+    }
+}
